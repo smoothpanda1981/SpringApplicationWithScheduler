3333a4b2caa73a30b16c246b5dac2719e68e4b8a
Yan Wang <yan.wang.ch@gmail.com>
2017-10-03 17:45:42 +0200

    First commit

---
+++ src/main/java/com/yan/wang/AppConfig.java
@@ -0,0 +1,19 @@
+package com.yan.wang;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+/**
+ * Created by ywang on 15.08.17.
+ */
+@Configuration
+@ComponentScan(basePackages="com.yan.wang")
+@EnableScheduling
+public class AppConfig {
+    @Scheduled(fixedRate=1500)
+    public void doTask() {
+        System.out.println("Do Task...");
+    }
+}
---
+++ src/main/java/com/yan/wang/SpringScheduler.java
@@ -0,0 +1,19 @@
+package com.yan.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by ywang on 15.08.17.
+ */
+@Configuration
+@EnableAutoConfiguration
+@EnableScheduling
+public class SpringScheduler {
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(AppConfig.class);
+    }
+}
