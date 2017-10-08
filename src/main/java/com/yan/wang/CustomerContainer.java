f253e217a9add46c301d13401599865f62fc1c41
Yan Wang <yan.wang.ch@gmail.com>
2017-10-09 01:41:23 +0200

    Change port, because 8080 already used.

---
+++ src/main/java/com/yan/wang/CustomerContainer.java
@@ -0,0 +1,14 @@
+package com.yan.wang;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomerContainer  implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.setPort(8099);
+    }
+}
